64603ff901b9ed83c67cb8a70d120ad4b56bc6b3
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2023-05-24 16:43:17 +0200

    fin de cours

--- Assets/Script/Ennemi.cs
+++ Assets/Script/Ennemi.cs
@@ -7,7 +7,7 @@ public class Ennemi : MonoBehaviour
     public Vector2Int Position;
     public float Offset;
 
-    int action = 0;
+    //int action = 0;
 
     public int LifePoints;
     public int ArmorPoints;
@@ -16,40 +16,45 @@ public class Ennemi : MonoBehaviour
     private void Start()
     {
         Character stats = new Character(LifePoints, ArmorPoints, DamagePoints);
-        StartCoroutine(IA());
+        //StartCoroutine(IA());
     }
 
-    public IEnumerator IA()
-    {
-        action = Random.Range(0, 3);
-        Move(action);
-        yield return new WaitForSeconds(1);
-        StartCoroutine(IA());
-    }
 
-    public void Move(int a)
-    {
-        if (a == 0)
-        {
-            if (GridData.gridData.isBlocked[Position.x, Position.y + 1] == false)
-                Position = new Vector2Int(Position.x, Position.y + 1);
-        }
-        else if (a == 1)
-        {
-            if (GridData.gridData.isBlocked[Position.x, Position.y - 1] == false)
-                Position = new Vector2Int(Position.x, Position.y - 1);
-        }
-        else if (a == 2)
-        {
-            if (GridData.gridData.isBlocked[Position.x + 1, Position.y] == false)
-                Position = new Vector2Int(Position.x + 1, Position.y);
-        }
-        else if (a == 3)
-        {
-            if (GridData.gridData.isBlocked[Position.x - 1, Position.y] == false)
-                Position = new Vector2Int(Position.x - 1, Position.y);
-        }
 
-        transform.position = new Vector3(Position.x + Offset, Position.y + Offset);
-    }
+
+
+
+    //public IEnumerator IA()
+    //{
+    //    action = Random.Range(0, 3);
+    //    Move(action);
+    //    yield return new WaitForSeconds(1);
+    //    StartCoroutine(IA());
+    //}
+
+    //public void Move(int a)
+    //{
+    //    if (a == 0)
+    //    {
+    //        if (GridData.gridData.isBlocked[Position.x, Position.y + 1] == false)
+    //            Position = new Vector2Int(Position.x, Position.y + 1);
+    //    }
+    //    else if (a == 1)
+    //    {
+    //        if (GridData.gridData.isBlocked[Position.x, Position.y - 1] == false)
+    //            Position = new Vector2Int(Position.x, Position.y - 1);
+    //    }
+    //    else if (a == 2)
+    //    {
+    //        if (GridData.gridData.isBlocked[Position.x + 1, Position.y] == false)
+    //            Position = new Vector2Int(Position.x + 1, Position.y);
+    //    }
+    //    else if (a == 3)
+    //    {
+    //        if (GridData.gridData.isBlocked[Position.x - 1, Position.y] == false)
+    //            Position = new Vector2Int(Position.x - 1, Position.y);
+    //    }
+
+    //    transform.position = new Vector3(Position.x + Offset, Position.y + Offset);
+    //}
 }
